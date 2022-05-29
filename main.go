6ce3e7f1113a11a7f5fa8a94bc820621d080a7e0
= <=>
2022-05-29 11:24:39 +0530

    ooki

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
-	fmt.Println(ImdbTtitle("tt0076759"))
 	if err := http.ListenAndServe(fetchPort(), nil); err != nil {
 		panic(err)
 	}
